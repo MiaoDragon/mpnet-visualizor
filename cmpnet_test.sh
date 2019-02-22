648bf304f69e04eb0f0ea5556358b146730493ba
MiaoDragon <innocentmyl@gmail.com>
2019-02-22 13:39:56 -0800

    parametrize testing

--- cmpnet_test.sh
+++ cmpnet_test.sh
@@ -3,3 +3,5 @@ python3 cmpnet_test.py --model_path ../CMPnet_res/c2d/ \
 --memory_strength 0.5 --n_memories 10000 \
 --n_tasks 1 --device 1 --data_path /media/arclabdl1/HD1/Ahmed/r-2d/ \
 --start_epoch 1 --memory_type res --env_type c2d --world_size 20
+--seen_N 10 --seen_NP 200 --seen_s 0 --seen_sp 4000 \
+--unseen_N 10 --seen_NP 200 --seen_s 100 --seen_sp 0
