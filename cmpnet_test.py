8c47dc97767db22e66e4388c336b830e84068481
MiaoDragon <innocentmyl@gmail.com>
2019-02-22 12:57:06 -0800

    added input dim to end2end

--- cmpnet_test.py
+++ cmpnet_test.py
@@ -41,8 +41,8 @@ def main(args):
     if torch.cuda.is_available():
         torch.cuda.set_device(args.device)
     if args.memory_type == 'res':
-        mpNet = End2EndMPNet(args.mlp_input_size, args.output_size, 'deep', \
-                    args.n_tasks, args.n_memories, args.memory_strength, args.grad_step)
+        mpNet = End2EndMPNet(args.total_input_size, args.AE_input_size, args.mlp_input_size, \
+                    args.output_size, 'deep', args.n_tasks, args.n_memories, args.memory_strength, args.grad_step)
     elif args.memory_type == 'rand':
         #mpNet = End2EndMPNet_rand(args.mlp_input_size, args.output_size, 'deep', \
         #            args.n_tasks, args.n_memories, args.memory_strength, args.grad_step)
@@ -115,6 +115,8 @@ parser.add_argument('--n_tasks', type=int, default=1,help='number of tasks')
 parser.add_argument('--n_memories', type=int, default=256, help='number of memories for each task')
 parser.add_argument('--memory_strength', type=float, default=0.5, help='memory strength (meaning depends on memory)')
 # Model parameters
+parser.add_argument('--total_input_size', type=int, default=2800+4, help='dimension of total input')
+parser.add_argument('--AE_input_size', type=int, default=2800, help='dimension of input to AE')
 parser.add_argument('--mlp_input_size', type=int , default=28+4, help='dimension of the input vector')
 parser.add_argument('--output_size', type=int , default=2, help='dimension of the input vector')
 parser.add_argument('--learning_rate', type=float, default=0.01)
---
+++ utility_s2d.py
@@ -0,0 +1,4 @@
+def normalize(x, bound):
+    return x
+def unnormalize(x, bound):
+    return x
