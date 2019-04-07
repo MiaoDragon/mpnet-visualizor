4cd0d7b11a1738440deeb4fed33e336da7441c44
MiaoDragon <innocentmyl@gmail.com>
2019-04-07 14:50:11 -0700

    try using new replan

--- gem_eval.py
+++ gem_eval.py
@@ -52,7 +52,7 @@ def eval_tasks(mpNet, test_data, true_file, path_file, IsInCollision, normalize_
                         step_sz = 0.03
                     elif (t > 3):
                         step_sz = 0.02
-                    path = neural_replan(mpNet, path, obc[i], obs[i], IsInCollision, \
+                    path = neural_replan2(mpNet, path, obc[i], obs[i], IsInCollision, \
                                          normalize_func, unnormalize_func, t==0, step_sz=step_sz)
                     path = lvc(path, obc[i], IsInCollision, step_sz=step_sz)
 
