4c62c9174a1fd25789d5464484a5da965bb9a2e0
MiaoDragon <innocentmyl@gmail.com>
2019-04-07 15:20:47 -0700

    added complex and simple

--- cmpnet_r2d_complex.sh
+++ cmpnet_r2d_complex.sh
@@ -1,7 +1,7 @@
-python3 plan_one_path.py --model_path ../PointMPnet_res/r2d/simple/ \
+python3 plan_one_path.py --model_path ../PointMPnet_res/r2d/complex/ \
 --learning_rate 0.01 \
 --memory_strength 0.5 --n_memories 10000 \
 --n_tasks 1 --device 1 --data_path /media/arclabdl1/HD1/Ahmed/rigid-body/dataset/ \
 --start_epoch 10 --memory_type res --env_type r2d --world_size 20 \
 --total_input_size 2806 --AE_input_size 2800 --mlp_input_size 34 --output_size 3 \
---env_idx 0 --path_idx 4013  --model_type simple
+--env_idx 0 --path_idx 4013  --model_type complex
