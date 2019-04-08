14c632456c93e701cadf213e948489ecbbcc21a2
MiaoDragon <innocentmyl@gmail.com>
2019-04-07 18:23:44 -0700

    lvc after 5 trails

--- gem_eval.py
+++ gem_eval.py
@@ -54,7 +54,8 @@ def eval_tasks(mpNet, test_data, true_file, path_file, IsInCollision, normalize_
                         step_sz = 0.02
                     path = neural_replan(mpNet, path, obc[i], obs[i], IsInCollision, \
                                          normalize_func, unnormalize_func, t==0, step_sz=step_sz)
-                    #path = lvc(path, obc[i], IsInCollision, step_sz=step_sz)
+                    if t >= 5:
+                        path = lvc(path, obc[i], IsInCollision, step_sz=step_sz)
                     pickle.dump(path, open(path_file+'_'+str(t), "wb" ))
                     if feasibility_check(path, obc[i], IsInCollision, step_sz=0.01):
                         fp = 1
