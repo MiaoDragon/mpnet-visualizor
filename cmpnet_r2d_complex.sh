3bca6b19050f35515ecfc1d3bae64817b5d5ed18
MiaoDragon <innocentmyl@gmail.com>
2019-04-07 20:32:48 -0700

    use 2 epoch

--- cmpnet_r2d_complex.sh
+++ cmpnet_r2d_complex.sh
@@ -2,6 +2,6 @@ python3 plan_one_path.py --model_path ../PointMPnet_res/r2d/complex/ \
 --learning_rate 0.01 \
 --memory_strength 0.5 --n_memories 10000 \
 --n_tasks 1 --device 1 --data_path /media/arclabdl1/HD1/Ahmed/rigid-body/dataset/ \
---start_epoch 4 --memory_type res --env_type r2d --world_size 20 \
+--start_epoch 2 --memory_type res --env_type r2d --world_size 20 \
 --total_input_size 2806 --AE_input_size 2800 --mlp_input_size 34 --output_size 3 \
 --env_idx 0 --path_idx 4002  --model_type complex
