0f5076b7904fed0b1e2c5a3e5c8befff15ab9168
MiaoDragon <innocentmyl@gmail.com>
2019-04-07 18:59:58 -0700

    dropout

--- Model/model_c2d.py
+++ Model/model_c2d.py
@@ -10,10 +10,10 @@ class MLP(nn.Module):
 	def __init__(self, input_size, output_size):
 		super(MLP, self).__init__()
 		self.fc = nn.Sequential(
-		nn.Linear(input_size, 1280),nn.PReLU(),nn.Dropout(),
-		nn.Linear(1280, 1024),nn.PReLU(),nn.Dropout(),
-		nn.Linear(1024, 768),nn.PReLU(),nn.Dropout(),
-		nn.Linear(768, 256),nn.PReLU(), nn.Dropout(),
+		nn.Linear(input_size, 1280),nn.PReLU(),#nn.Dropout(),
+		nn.Linear(1280, 1024),nn.PReLU(),#nn.Dropout(),
+		nn.Linear(1024, 768),nn.PReLU(),#nn.Dropout(),
+		nn.Linear(768, 256),nn.PReLU(), #nn.Dropout(),
 		nn.Linear(256, 256),nn.PReLU(), nn.Dropout(),
 		nn.Linear(256, 128),nn.PReLU(), nn.Dropout(),
 		nn.Linear(128, 64),nn.PReLU(), nn.Dropout(),
