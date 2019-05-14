57b2ceba47cce040f0d15cfcac19e1fb0889e6d9
MiaoDragon <innocentmyl@gmail.com>
2019-05-14 13:33:06 -0700

    model

--- Model/model.py
+++ Model/model.py
@@ -6,22 +6,22 @@ from torch.autograd import Variable
 
 
 class MLP(nn.Module):
-    def __init__(self, input_size, output_size):
-        super(MLP, self).__init__()
-        self.fc = nn.Sequential(
-        nn.Linear(input_size, 1280),nn.PReLU(),#nn.Dropout(),
-        nn.Linear(1280, 1024),nn.PReLU(),#nn.Dropout(),
-        nn.Linear(1024, 896),nn.PReLU(),#nn.Dropout(),
-        nn.Linear(896, 768),nn.PReLU(),#nn.Dropout(),
-        nn.Linear(768, 512),nn.PReLU(),#nn.Dropout(),
-        nn.Linear(512, 384),nn.PReLU(),nn.Dropout(p=0.2),
-        nn.Linear(384, 256),nn.PReLU(), nn.Dropout(p=0.2),
-        nn.Linear(256, 256),nn.PReLU(), nn.Dropout(p=0.2),
-        nn.Linear(256, 128),nn.PReLU(), nn.Dropout(p=0.2),
-        nn.Linear(128, 64),nn.PReLU(), nn.Dropout(p=0.2),
-        nn.Linear(64, 32),nn.PReLU(),
-        nn.Linear(32, output_size))
+	def __init__(self, input_size, output_size):
+		super(MLP, self).__init__()
+		self.fc = nn.Sequential(
+		nn.Linear(input_size, 1280),nn.PReLU(),nn.Dropout(),
+		nn.Linear(1280, 1024),nn.PReLU(),nn.Dropout(),
+		nn.Linear(1024, 896),nn.PReLU(),nn.Dropout(),
+		nn.Linear(896, 768),nn.PReLU(),nn.Dropout(),
+		nn.Linear(768, 512),nn.PReLU(),nn.Dropout(),
+		nn.Linear(512, 384),nn.PReLU(),nn.Dropout(),
+		nn.Linear(384, 256),nn.PReLU(), nn.Dropout(),
+		nn.Linear(256, 256),nn.PReLU(), nn.Dropout(),
+		nn.Linear(256, 128),nn.PReLU(), nn.Dropout(),
+		nn.Linear(128, 64),nn.PReLU(), nn.Dropout(),
+		nn.Linear(64, 32),nn.PReLU(),
+		nn.Linear(32, output_size))
 
-    def forward(self, x):
-        out = self.fc(x)
-        return out
+	def forward(self, x):
+		out = self.fc(x)
+		return out
