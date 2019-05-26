be14060c6e6afa33efc5f0ae662d1f82a45e8bf9
MiaoDragon <innocentmyl@gmail.com>
2019-05-26 01:18:00 -0700

    no planner

--- ompl_mpnet.sh
+++ ompl_mpnet.sh
@@ -1,3 +1,3 @@
 python ompl_mpnet.py --model_path ../ompl_res/mpnet_compare/ --env_idx 0 --path_idx 4000 \
 --N 100 --NP 200 --data_path /media/arclabdl1/HD1/YLmiao/mpnet/data/simple/ --env_type s2d \
---planner informedrrtstar --data_type seen
+--data_type seen
