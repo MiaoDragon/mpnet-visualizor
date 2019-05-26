fa8ac0286eef860af472f7d1729dec674a82a0a3
MiaoDragon <innocentmyl@gmail.com>
2019-05-26 01:26:26 -0700

    import and model

--- ompl_mpnet.py
+++ ompl_mpnet.py
@@ -1,3 +1,11 @@
+from Model.GEM_end2end_model import End2EndMPNet
+#from GEM_end2end_model_rand import End2EndMPNet as End2EndMPNet_rand
+import Model.model as model
+import Model.model_c2d as model_c2d
+import Model.AE.CAE_r3d as CAE_r3d
+import Model.AE.CAE as CAE_2d
+import torch
+from torch.autograd import Variable
 from ompl import base as ob
 from ompl import geometric as og
 import plan_c2d, plan_s2d, plan_r2d, plan_r3d
@@ -9,6 +17,7 @@ import time
 import os
 import numpy as np
 from plan_general import *
+from utility import *
 import utility_s2d, utility_c2d, utility_r3d, utility_r2d
 def eval_tasks(mpNet, test_data, env_idx, path_idx, IsInCollision, normalize_func = lambda x:x, unnormalize_func=lambda x: x, time_flag=False):
     obc, obs, paths, path_lengths = test_data
@@ -125,6 +134,8 @@ def plan(args):
         time_limit = 20.
         normalize = utility_s2d.normalize
         unnormalize = utility_s2d.unnormalize
+        CAE = CAE_2d
+        MLP = model.MLP
     elif args.env_type == 'c2d':
         data_loader = data_loader_2d
         IsInCollision = plan_c2d.IsInCollision
@@ -137,6 +148,8 @@ def plan(args):
         time_limit = 20.
         normalize = utility_c2d.normalize
         unnormalize = utility_c2d.unnormalize
+        CAE = CAE_2d
+        MLP = model_c2d.MLP
     elif args.env_type == 'r2d':
         data_loader = data_loader_r2d
         IsInCollision = plan_r2d.IsInCollision
@@ -157,6 +170,25 @@ def plan(args):
         bounds.setHigh(20)
         space.setBounds(bounds)
         time_limit = 20.
+    mpNet = End2EndMPNet(args.total_input_size, args.AE_input_size, args.mlp_input_size, \
+                args.output_size, 'deep', args.n_tasks, args.n_memories, args.memory_strength, args.grad_step, \
+                CAE, MLP)
+    model_path='cmpnet_epoch_%d.pkl' %(args.start_epoch)
+    if args.start_epoch > 0:
+        load_net_state(mpNet, os.path.join(args.model_path, model_path))
+        torch_seed, np_seed, py_seed = load_seed(os.path.join(args.model_path, model_path))
+        # set seed after loading
+        torch.manual_seed(torch_seed)
+        np.random.seed(np_seed)
+        random.seed(py_seed)
+
+    if torch.cuda.is_available():
+        mpNet.cuda()
+        mpNet.mlp.cuda()
+        mpNet.encoder.cuda()
+        mpNet.set_opt(torch.optim.Adagrad, lr=args.learning_rate)
+    if args.start_epoch > 0:
+        load_opt_state(mpNet, os.path.join(args.model_path, model_path))
 
     test_data = data_loader.load_test_dataset(N=args.N, NP=args.NP, s=args.env_idx, sp=args.path_idx, folder=args.data_path)
     obcs, obs, paths, path_lengths = test_data
@@ -285,5 +317,19 @@ parser.add_argument('--data_path', type=str, default='../data/simple/')
 parser.add_argument('--env_type', type=str, default='s2d')
 #parser.add_argument('--planner', type=str, default='bitstar')
 parser.add_argument('--data_type', type=str, default='seen')
+
+
+parser.add_argument('--n_tasks', type=int, default=1,help='number of tasks')
+parser.add_argument('--n_memories', type=int, default=256, help='number of memories for each task')
+parser.add_argument('--memory_strength', type=float, default=0.5, help='memory strength (meaning depends on memory)')
+parser.add_argument('--total_input_size', type=int, default=2800+4, help='dimension of total input')
+parser.add_argument('--AE_input_size', type=int, default=2800, help='dimension of input to AE')
+parser.add_argument('--mlp_input_size', type=int , default=28+4, help='dimension of the input vector')
+parser.add_argument('--output_size', type=int , default=2, help='dimension of the input vector')
+parser.add_argument('--learning_rate', type=float, default=0.01)
+parser.add_argument('--device', type=int, default=0, help='cuda device')
+parser.add_argument('--start_epoch', type=int, default=0)
+parser.add_argument('--memory_type', type=str, default='res', help='res for reservoid, rand for random sampling')
+parser.add_argument('--world_size', type=int, default=50, help='boundary of world')
 args = parser.parse_args()
 plan(args)
--- ompl_mpnet.sh
+++ ompl_mpnet.sh
@@ -1,3 +1,3 @@
 python ompl_mpnet.py --model_path ../ompl_res/mpnet_compare/ --env_idx 0 --path_idx 4000 \
---N 100 --NP 200 --data_path /media/arclabdl1/HD1/YLmiao/mpnet/data/simple/ --env_type s2d \
---data_type seen
+--N 100 --NP 200 --data_path /media/arclabdl1/HD1/Ahmed/r-2d/ --env_type c2d \
+--data_type seen --learning_rate 0.01 --memory_strength 0.5 --n_tasks 1 --start_epoch 1
