9ac43597b670df32e7db1e63eb81a3d1163cad9e
MiaoDragon <innocentmyl@gmail.com>
2019-05-29 11:13:33 -0700

    removed informedrrtstar for s2d and c2d

--- ompl_plan.sh
+++ ompl_plan.sh
@@ -1,15 +1,3 @@
-python ompl_plan_general.py --model_path ../ompl_res/informedrrt/s2d/ --env_idx 0 --path_idx 4000 \
---N 100 --NP 200 --data_path /media/arclabdl1/HD1/YLmiao/mpnet/data/simple/ --env_type s2d \
---planner informedrrtstar --data_type seen
-python ompl_plan_general.py --model_path ../ompl_res/informedrrt/s2d/ --env_idx 100 --path_idx 0 \
---N 10 --NP 2000 --data_path /media/arclabdl1/HD1/YLmiao/mpnet/data/simple/ --env_type s2d \
---planner informedrrtstar --data_type unseen
-python ompl_plan_general.py --model_path ../ompl_res/informedrrt/c2d/ --env_idx 0 --path_idx 4000 \
---N 100 --NP 200 --data_path /media/arclabdl1/HD1/Ahmed/r-2d/ --env_type c2d \
---planner informedrrtstar --data_type seen
-python ompl_plan_general.py --model_path ../ompl_res/informedrrt/c2d/ --env_idx 100 --path_idx 0 \
---N 10 --NP 2000 --data_path /media/arclabdl1/HD1/Ahmed/r-2d/ --env_type c2d \
---planner informedrrtstar --data_type unseen
 python ompl_plan_general.py --model_path ../ompl_res/informedrrt/r2d/ --env_idx 0 --path_idx 4000 \
 --N 100 --NP 200 --data_path /media/arclabdl1/HD1/Ahmed/rigid-body/dataset/ --env_type r2d \
 --planner informedrrtstar --data_type seen
