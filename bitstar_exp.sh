cab6e067d52ed2f9ea3e8d9f8e13deeea8f6304a
MiaoDragon <innocentmyl@gmail.com>
2019-06-17 18:38:34 -0700

    rewire factor

--- bitstar_exp.sh
+++ bitstar_exp.sh
@@ -1,18 +1,15 @@
 python bitstar_plan_general.py --model_path ../ompl_res/bitstar/r2d/ --env_idx 0 --path_idx 4000 \
 --N 10 --NP 2 --data_path /media/arclabdl1/HD1/Ahmed/rigid-body/dataset/ --env_type r2d \
---planner bitstar --data_type seen --samples 200 --rewire 1.1
-python bitstar_plan_general.py --model_path ../ompl_res/bitstar/r2d/ --env_idx 0 --path_idx 4000 \
---N 10 --NP 2 --data_path /media/arclabdl1/HD1/Ahmed/rigid-body/dataset/ --env_type r2d \
---planner bitstar --data_type seen --samples 200 --rewire 0.5
-python bitstar_plan_general.py --model_path ../ompl_res/bitstar/r2d/ --env_idx 0 --path_idx 4000 \
---N 10 --NP 2 --data_path /media/arclabdl1/HD1/Ahmed/rigid-body/dataset/ --env_type r2d \
---planner bitstar --data_type seen --samples 200 --rewire 0.8
-python bitstar_plan_general.py --model_path ../ompl_res/bitstar/r2d/ --env_idx 0 --path_idx 4000 \
---N 10 --NP 2 --data_path /media/arclabdl1/HD1/Ahmed/rigid-body/dataset/ --env_type r2d \
---planner bitstar --data_type seen --samples 200 --rewire 1.5
-python bitstar_plan_general.py --model_path ../ompl_res/bitstar/r2d/ --env_idx 0 --path_idx 4000 \
---N 10 --NP 2 --data_path /media/arclabdl1/HD1/Ahmed/rigid-body/dataset/ --env_type r2d \
---planner bitstar --data_type seen --samples 200 --rewire 2.0
-python bitstar_plan_general.py --model_path ../ompl_res/bitstar/r2d/ --env_idx 0 --path_idx 4000 \
---N 10 --NP 2 --data_path /media/arclabdl1/HD1/Ahmed/rigid-body/dataset/ --env_type r2d \
 --planner bitstar --data_type seen --samples 200 --rewire 3.0
+python bitstar_plan_general.py --model_path ../ompl_res/bitstar/r2d/ --env_idx 0 --path_idx 4000 \
+--N 10 --NP 2 --data_path /media/arclabdl1/HD1/Ahmed/rigid-body/dataset/ --env_type r2d \
+--planner bitstar --data_type seen --samples 200 --rewire 5.0
+python bitstar_plan_general.py --model_path ../ompl_res/bitstar/r2d/ --env_idx 0 --path_idx 4000 \
+--N 10 --NP 2 --data_path /media/arclabdl1/HD1/Ahmed/rigid-body/dataset/ --env_type r2d \
+--planner bitstar --data_type seen --samples 200 --rewire 10.0
+python bitstar_plan_general.py --model_path ../ompl_res/bitstar/r2d/ --env_idx 0 --path_idx 4000 \
+--N 10 --NP 2 --data_path /media/arclabdl1/HD1/Ahmed/rigid-body/dataset/ --env_type r2d \
+--planner bitstar --data_type seen --samples 200 --rewire 15.0
+python bitstar_plan_general.py --model_path ../ompl_res/bitstar/r2d/ --env_idx 0 --path_idx 4000 \
+--N 10 --NP 2 --data_path /media/arclabdl1/HD1/Ahmed/rigid-body/dataset/ --env_type r2d \
+--planner bitstar --data_type seen --samples 200 --rewire 40.0
